c2567705fe026bebd88070f65ea7ea3eba1322a0
mradicx <161376437+mradicx@users.noreply.github.com>
2026-05-09 21:46:12 +0100

    Fix: parameterise login SQL in db.py (SR9)
    Replaced with native psycopg2 parameterisation:
      cur.execute("... WHERE username = %s", (username,))
    
    Maps to inventory row 11 (SQLi - auth query).

--- web/app/db.py
+++ web/app/db.py
@@ -1,6 +1,9 @@
 from . import utils
 
 def get_user_by_username(cur, username):
-    query = utils.prepare_query("SELECT id, username, password, is_disabled FROM users WHERE username='%s'", username)
-    cur.execute(query)
+    cur.execute(
+        "SELECT id, username, password, is_disabled "
+        "FROM users WHERE username = %s",
+        (username,),
+    )
     return cur.fetchone()
